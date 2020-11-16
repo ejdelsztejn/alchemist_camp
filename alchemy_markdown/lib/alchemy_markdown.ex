8a98711085884b5c7033da61338ba24ba7ddb571
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-11-16 10:16:37 -0500

    Add test for big tag

--- alchemy_markdown/lib/alchemy_markdown.ex
+++ alchemy_markdown/lib/alchemy_markdown.ex
@@ -1,9 +1,5 @@
 defmodule AlchemyMarkdown do
   def to_html(markdown) do
-    Earmark.as_html(markdown) || ""), %EarMark.Options(smartypants: false)
-  end
-
-  def hello do
-    :world
+    Earmark.as_html!((markdown) || "", %Earmark.Options{smartypants: false})
   end
 end
--- alchemy_markdown/test/alchemy_markdown_test.exs
+++ alchemy_markdown/test/alchemy_markdown_test.exs
@@ -2,12 +2,13 @@ defmodule AlchemyMarkdownTest do
   use ExUnit.Case
   doctest AlchemyMarkdown
 
-  test "greets the world" do
-    assert AlchemyMarkdown.hello() == :world
-  end
-
   test "italicizes" do
     string = "Something *important*"
-    assert AlchemyMarkdown.to_html(string) == "Something <em>important</em>"
+    assert AlchemyMarkdown.to_html(string) =~ "Something <em>important</em>"
+  end
+
+  test "expands big tags" do
+    str = "Some ++big++ words!"
+    assert AlchemyMarkdown.to_html(str) =~ "<big>big</big> words"
   end
 end
