785bbf355a5316b9554134b11cfbfedc620aebbb
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-11-16 10:48:19 -0500

    Create test string and output.html file

--- alchemy_markdown/lib/alchemy_markdown.ex
+++ alchemy_markdown/lib/alchemy_markdown.ex
@@ -3,6 +3,7 @@ defmodule AlchemyMarkdown do
     Earmark.as_html!((markdown) || "", %Earmark.Options{smartypants: false})
       |> big
       |> small
+      |> hrs
   end
 
   def small(text) do
@@ -14,6 +15,26 @@ defmodule AlchemyMarkdown do
   end
 
   def hrs(text) do
-    Regex.replace(~r{\r\n|\r|\n([-*])( *\1 *)+\1}, text, "<hr />")
+    Regex.replace(~r{(^|\r\n|\r|\n)([-*])( *\2 *)+\2}, text, "\\1<hr />")
+  end
+
+  def test_str do
+    """
+    # Main title of my doc
+
+    ## A less important header
+
+    I *really* disagree with you!
+
+    You can **strongly** emphasize text
+
+    You can make lists of
+
+    1) One
+    2) Two
+    3) Three
+
+    things ++big things!++ --small things!--
+    """
   end
 end
