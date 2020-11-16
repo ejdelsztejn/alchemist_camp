acb928357066a91819482eeb1ae264a1b7f70457
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-11-16 10:18:41 -0500

    Add big and small functions; Pipe to_html into big and small functions

--- alchemy_markdown/lib/alchemy_markdown.ex
+++ alchemy_markdown/lib/alchemy_markdown.ex
@@ -1,5 +1,15 @@
 defmodule AlchemyMarkdown do
   def to_html(markdown) do
     Earmark.as_html!((markdown) || "", %Earmark.Options{smartypants: false})
+      |> big
+      |> small
+  end
+
+  def small(text) do
+    Regex.replace(~r/\-\-(.*)\-\-/, text, "<small>\\1</small>")
+  end
+
+  def big(text) do
+    Regex.replace(~r/\+\+(.*)\+\+/, text, "<big>\\1</big>")
   end
 end
